28a91ffac574c6114df021a84a7216b4e20ed0fc
tanweihao <tanweihao88@gmail.com>
2013-10-10 14:26:23 -0400

    trying to get it to run on heroku

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ var router = express();
 var server = http.createServer(router);
 router.use(express.static(path.resolve(__dirname, 'client')));
 
+// Epxress app configurations
+router.configure(function () {
+    router.use(router.router);
+    router.use(express.static(__dirname + '/client'));
+});
+
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
   console.log("Server running at", addr.address + ":" + addr.port);

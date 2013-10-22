22363f52015b0ae76fa26c4773b5a08cf4403d3a
OpenShift guest <tanweihao88@gmail.com>
2013-10-22 14:58:56 -0400

    Added log in and register. test.

--- client/javascripts/application.js
+++ client/javascripts/application.js
@@ -34477,7 +34477,7 @@ plots, you can just fix the size of their placeholders.
                 var expireDate = new Date().addHours(4);
                 //document.cookie = username+'='+json+';expires='+expireDate;
                 createCookie("username",username,3);
-                createCookie("orgId",json,3);
+                createCookie("orgId",json.result,3);
                 window.location.replace("fullcalendar.html");
     	    }
         });

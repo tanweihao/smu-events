2e0365a82368033b8aa6c0c891b71369c9a545fd
OpenShift guest <tanweihao88@gmail.com>
2013-11-14 20:17:46 -0500

    Integrated livelabs location

--- routes/apis/event_add.js
+++ routes/apis/event_add.js
@@ -4,24 +4,28 @@ module.exports = function(args) {
     app.post('/api/events/add_event', function(req, res) {
         var userCollection = db.collection('users'),
             eventCollection = db.collection('events');
-
+        
         userCollection.findOne({
             id: req.body.org_id
         }, function(err, user) {
-            if (!err && typeof user != 'undefined') {
+            if (!err) {
                 eventCollection.count(function(err, docs) {
                     if (!err) {
-                        var id = docs + 1;
-
+                        var id = docs + 1,
+                            start_date = new Date(req.body.start_date);
+                        start_date.setHours(start_date.getHours()+6);
+                        var dateStr = start_date.getDate() +""+ (start_date.getMonth()+1) + start_date.getFullYear() + start_date.getHours();
+                        
                         eventCollection.insert({
                             id: id,
                             event_name: req.body.event_name,
                             org_name: user.username,
                             org_id: req.body.org_id,
-                            start_date: new Date(req.body.start_date),
+                            start_date: start_date,
                             end_date: new Date(req.body.end_date),
                             venue: req.body.venue,
                             description: req.body.description,
+                            code: dateStr,
                             signups: [{
                                 id: 1,
                                 registered: false
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express'),
     http = require("http"),
     url = require("url"),
     fs = require("fs"),
+    request = require('request'),
+    crypto = require('crypto'),
     rootDir = __dirname,
     mongodb = require("mongodb"),
     MongoClient = mongodb.MongoClient,
@@ -21,14 +23,74 @@ MongoClient.connect(connection_string, function (err, db) {
             app.use(app.router);
         });
         
+        //Generate Login hashes
+        var userSHA = crypto.createHash('sha1'),
+            passwordSHA = crypto.createHash('sha1');
+        
+        userSHA.update("weihao.tan.2010@sis.smu.edu.sg");
+        passwordSHA.update("60116364");
+        
+        var userHash = userSHA.digest('hex'),
+            passwordHash = passwordSHA.digest('hex');
+        
         var args = {
             app: app,
             url: url,
+            http: http,
+            request: request,
+            crypto: crypto,
+            userHash: userHash,
+            passwordHash: passwordHash,
             fs: fs,
             db: db,
             io: io,
-            rootDir: rootDir
+            rootDir: rootDir,
+            eventList: {}
         };
+        
+        //Populating the event list
+        var eventCollection = db.collection('events'),
+            date = new Date();
+        date.setHours(date.getHours()+13);
+        var dateStr = date.getDate() +""+ (date.getMonth()+1) + date.getFullYear() + date.getHours();
+        args.eventList[dateStr] = [];
+        
+        // console.log(dateStr);
+        eventCollection.find({
+            code: dateStr
+        }).toArray(function(err, events) {
+            if (!err) {
+                events.forEach(function(event) {
+                    args.eventList[dateStr].push({
+                        id: event.id,
+                        signups: event.signups
+                    });
+                });
+            }
+        });
+        
+        //Logging into Livelabs API
+        request.post('http://athena.smu.edu.sg/hestia/livelabs/index.php/authenticate/login_others', {
+            form: {
+                username: userHash,
+                password: passwordHash,
+                appid: "176110"
+            },
+            jar: true
+        }, function(error, res, data) {
+            console.log(data);
+        });
+        
+        //Looping through the event list
+        setInterval(function() {
+            args.eventList[dateStr].forEach(function(event) {
+                event.signups.forEach(function(user) {
+                    if (!user.registered) {
+                        console.log(user.id);
+                    }
+                })
+            });
+        }, 5000);
         
         require('./routes/routes')(args);
         server.listen(process.env.PORT || 3000, function() {
